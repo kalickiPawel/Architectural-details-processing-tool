61c1ef1a8ee077669b396b67880dabf65ba4aad0
Paweł Kalicki <kalicki.paul@icloud.com>
2020-01-04 00:21:38 +0100

    Added connection to Flask API

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { People } from './components/People';
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/people').then(response =>
+      response.json().then(data => {
+        setPeople(data);
+      })
+    );
+  }, []);
+
+  console.log(people);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <People people={people} />
     </div>
   );
 }
