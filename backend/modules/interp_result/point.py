6c988b00c99d4bdf96edcb6a61285ef0fd1b0d67
Paweł Kalicki <kalicki.paul@icloud.com>
2020-01-11 19:31:59 +0100

    Added interpolation module

---
+++ backend/modules/interp_result/point.py
@@ -0,0 +1,19 @@
+from scipy.interpolate import interp1d
+import numpy as np
+
+
+def get_interp_result(coords, method):
+    x, y = coords
+
+    n = len(x)
+    s = [i for i in range(n)]
+    t = np.linspace(0, n-1, 100)
+
+    u = interp1d(s, x, kind=method)
+    v = interp1d(s, y, kind=method)
+
+    X, Y = u(t), v(t)
+
+    interp_points = [{'x': X[i], 'y':Y[i]} for i in range(0, len(X))]
+
+    return interp_points
