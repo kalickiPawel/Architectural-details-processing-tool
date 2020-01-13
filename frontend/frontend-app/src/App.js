6c7ebf7c26e5c0c1c942dae8fa24474cbd5f5f23
Paweł Kalicki <kalicki.paul@icloud.com>
2020-01-13 02:11:32 +0100

    Changed 'class' to 'className' in <div>

--- frontend/frontend-app/src/App.js
+++ frontend/frontend-app/src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div class="row">
-          <div class="col-sm-4">.col-sm-4</div>
-          <div class="col-sm-4">.col-sm-4</div>
-          <div class="col-sm-4"><InterpChart /></div>
+        <div className="row">
+          <div className="col-sm-4">.col-sm-4</div>
+          <div className="col-sm-4">.col-sm-4</div>
+          <div className="col-sm-4"><InterpChart /></div>
         </div>
       </div>
     );
