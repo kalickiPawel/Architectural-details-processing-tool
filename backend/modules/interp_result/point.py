92e1c66d20dcdd6b2d3eb98a15383e5398433cc8
Paweł Kalicki <kalicki.paul@icloud.com>
2020-01-30 14:35:25 +0100

    Added interp1d method for interpolation

--- backend/modules/interp_result/point.py
+++ backend/modules/interp_result/point.py
@@ -1,19 +1,47 @@
-from scipy.interpolate import interp1d
+from scipy.interpolate import griddata, interp1d
 import numpy as np
 
 
-def get_interp_result(coords, method):
-    x, y = coords
+def get_interp_result(coords, method, grid_size=40):
 
-    n = len(x)
-    s = [i for i in range(n)]
-    t = np.linspace(0, n-1, n*50)
+    x, y, z = coords
+
+    N = len(x)
+    s = [i for i in range(N)]
+    t = np.linspace(0, N-1, N*6)
 
     u = interp1d(s, x, kind=method)
     v = interp1d(s, y, kind=method)
+    w = interp1d(s, z, kind=method)
 
-    X, Y = u(t), v(t)
+    x, y, z = u(t), v(t), w(t)
 
-    interp_points = [{'x': X[i], 'y':Y[i]} for i in range(0, len(X))]
+    n = len(x)
+
+    x = np.asarray(x).reshape((n, 1))
+    y = np.asarray(y).reshape((n, 1))
+    z = np.asarray(z).reshape((n, 1))
+
+    minx, maxx = min(x)[0], max(x)[0]
+    miny, maxy = min(y)[0], max(y)[0]
+
+    xx, yy = np.mgrid[minx:maxx:grid_size*1j,
+                      miny:maxy:grid_size*1j]
+
+    xy = np.hstack((x, y))
+    ZZ_interp = griddata(xy, z, (xx, yy), method=method)
+
+    xx = xx.reshape((grid_size*grid_size, ))
+    yy = yy.reshape((grid_size*grid_size, ))
+    ZZ_interp = ZZ_interp.reshape((grid_size*grid_size, ))
+
+    interp_points = [
+        {
+            'x': x,
+            'y': y,
+            'z': z
+        }
+        for x, y, z in zip(xx, yy, ZZ_interp)
+        if not np.isnan(z)]
 
     return interp_points
