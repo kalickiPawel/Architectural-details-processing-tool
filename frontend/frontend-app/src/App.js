be994be957f0a858f49dfa07cd67edb7a0d21f3b
Paweł Kalicki <kalicki.paul@icloud.com>
2020-01-30 14:37:52 +0100

    Implemented redux store and data transfer for app

--- frontend/frontend-app/src/App.js
+++ frontend/frontend-app/src/App.js
@@ -2,22 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import InputZTable from './Components/InputZTable';
+import InputXYPoints from './Components/InputXYPoints';
+import InputMethodRadio from './Components/InputMethodRadio';
 import InterpChart from './Components/InterpChart';
-import InputFrontImage from './Components/InputFrontImage';
+import SendButton from './Components/SendButton';
 
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="row">
-          <InputFrontImage />
-          <div className="col-sm-4">.col-sm-4</div>
-          <div className="col-sm-4"><InterpChart /></div>
+      <div className="container" >
+
+        <div className="row bg-light">
+          <div className="col">
+            <nav className="navbar navbar-light">
+              <h1 className="display-4">WebInterpTool</h1>
+            </nav>
+          </div>
         </div>
+
+        <div className="row align-items-center">
+          <div className="col-8">
+            <div className="containter">
+              <div className="row bg-content align-items-center">
+                <div className="col-6 pt-3 pb-3">
+                  <InputXYPoints />
+                </div>
+                <div className="col-6">
+                  <div className="row pt-3 pb-3">
+                    <InputZTable xList={this.props.data.x} yList={this.props.data.y} />
+                  </div>
+                  <div className="row pt-3 pb-3 text-center">
+                    <InputMethodRadio />
+                  </div>
+                  <div className="row pt-3 pb-3 text-center">
+                    <div className="container">
+                      <SendButton />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 pt-3 pb-3">
+            <InterpChart xList={this.props.data.x} yList={this.props.data.y} />
+          </div>
+        </div >
       </div >
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    data: state.mouseCoords
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- frontend/frontend-app/src/index.js
+++ frontend/frontend-app/src/index.js
@@ -4,7 +4,125 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+
+import { createStore, /*applyMiddleware*/ } from 'redux';
+//import { createLogger } from 'redux-logger';
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_X':
+            state = {
+                ...state,
+                mouseCoords: {
+                    ...state.mouseCoords,
+                    x: [...state.mouseCoords.x, action.payload]
+                }
+            }
+            break;
+        case 'ADD_Y':
+            state = {
+                ...state,
+                mouseCoords: {
+                    ...state.mouseCoords,
+                    y: [...state.mouseCoords.y, action.payload]
+                }
+            }
+            break;
+        case 'ADD_Z':
+            let z_values = [];
+            for (var element in action.payload) {
+                z_values.push(parseFloat(action.payload[element]));
+            }
+            state = {
+                ...state,
+                mouseCoords: {
+                    ...state.mouseCoords,
+                    z: z_values
+                }
+            }
+            break;
+        case 'ADD_METHOD':
+            state = {
+                ...state,
+                method: action.payload
+            }
+            break;
+        case 'ADD_INTERP':
+            let x_table = [];
+            let y_table = [];
+            let z_table = [];
+            action.payload.map((value) => {
+                x_table.push(value.x);
+                y_table.push(value.y);
+                z_table.push(value.z);
+                return 0;
+            });
+            state = {
+                ...state,
+                interpCoords: {
+                    ...state.interpCoords,
+                    x: x_table,
+                    y: y_table,
+                    z: z_table
+                }
+            };
+            break;
+        case 'REMOVE_X':
+            state = {
+                ...state,
+                mouseCoords: {
+                    ...state.mouseCoords,
+                    x: [...state.mouseCoords.x.slice(0, action.payload)]
+                }
+            }
+            break;
+        case 'REMOVE_Y':
+            state = {
+                ...state,
+                mouseCoords: {
+                    ...state.mouseCoords,
+                    y: [...state.mouseCoords.y.slice(0, action.payload)]
+                }
+            }
+            break;
+        case 'REMOVE_Z':
+            state = {
+                ...state,
+                mouseCoords: {
+                    ...state.mouseCoords,
+                    z: [...state.mouseCoords.z.slice(0, action.payload)]
+                }
+            }
+            break;
+        default:
+            break;
+    }
+    return state;
+};
+
+const initialState = {
+    mouseCoords: { x: [], y: [], z: [] },
+    interpCoords: { x: [], y: [], z: [] },
+    method: "",
+};
+
+// let logger = createLogger()
+
+let store = createStore(
+    reducer,
+    // applyMiddleware(logger)
+)
+
+store.subscribe(() => {
+    // console.log(store.getState())
+});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
